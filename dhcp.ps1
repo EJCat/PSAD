cc6a9da1f2763daea66a0cf1f7920ec9ccb77656
EJC <ejc3336@rit.edu>
2025-11-18 14:14:37 -0500

    deploy dhcp

--- dhcp.ps1
+++ dhcp.ps1
@@ -1,3 +1,14 @@
 $fqdn = $($env:COMPUTERNAME).$($env:USERDNSDOMAIN)
 $env:ip = (Test-Connection -ComputerName $env:COMPUTERNAME -Count 1).IPV4Address.IPAddressToString
 Add-DhcpServerInDC -DnsName $fqdn -IPAddress $env:ip
+
+$start = Read-Host "Enter DHCP start range"
+$end = Read-Host "Enter DHCP end range"
+$scopeid = Read-Host "Enter Scope ID (network ID)"
+Add-DhcpServerv4Scope -Name "Network" -StartRange $start -EndRange $end -SubnetMask 255.255.255.0
+
+Set-DhcpServerv4OptionValue -ScopeId $scopeid -OptionId 6 -Value $env:ip, "8.8.8.8"
+
+$exstart = Read-Host "Enter DHCP exclusion start range"
+$exend = Read-Host "Enter DHCP exclusion end range"
+Add-DhcpServerv4ExclusionRange -ScopeId $scopeid -StartRange $exstart -EndRange $exend
