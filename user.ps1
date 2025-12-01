749ac3b223cea9abb65887187b0e57baee0a1a9a
EJC <ejc3336@rit.edu>
2025-12-01 13:38:47 -0500

    add comments

--- user.ps1
+++ user.ps1
@@ -12,3 +12,7 @@ $user = @{
 }
 Import-Module ActiveDirectory
 New-ADUser @user
+
+# Use following command in elevated powershell on client to join domain
+# Add-Computer –DomainName (DOMAINNAMEHERE) –Credential (Get-Credential)
+# e.g. Add-Computer –DomainName gpavks.com –Credential (Get-Credential)
